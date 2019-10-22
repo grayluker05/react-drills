59e6a8368983ab6a4443574c4059ed1d8ba9462c
grayluker05 <graysonluker@gmail.com>
2019-10-22 16:02:45 -0600

    finished problem 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,29 +1,59 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// class App extends Component {
+// constructor(){
+//   super();
+
+//   this.state={
+//     message: ''
+//   }
+// }
+
+// handleChange(value) {
+//   this.setState({
+//     message: value
+//   })
+// }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <input onChange={(e) => this.handleChange(e.target.value)} type='text'/>
+//         <p>{this.state.message}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, {Component} from 'react';
 
 class App extends Component {
-constructor(){
-  super();
+  constructor(){
+    super();
 
-  this.state={
-    message: ''
+    this.state = {
+      message: ''
+    }
   }
-}
 
-handleChange(value) {
-  this.setState({
-    message: value
-  })
-}
+  handleChange(value){
+    this.setState({
+      message: value
+    })
+  }
 
-  render() {
-    return (
-      <div className="App">
-        <input onChange={(e) => this.handleChange(e.target.value)} type='text'/>
+  render(){
+    return(
+      <div className='App'>
+        <input onChange={(e) => this.handleChange(e.target.value)} value='text'/>
         <p>{this.state.message}</p>
       </div>
-    );
+    )
   }
 }
 
